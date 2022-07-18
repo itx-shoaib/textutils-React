6840e9234ac784f98ff8f1872d4b5c518ab9d6ba
Shoaib Jamil <shoaibjamil43@gmail.com>
2022-07-18 13:33:19 +0500

    Adding lower to case,word and character counting,minutes read and preview logic

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -8,16 +8,28 @@ export default function TextForm(props) {
     const handleUpChange = (event)=>{
         setText(event.target.value);
     }
+    const handleloClick = ()=>{
+      let newText = text.toLowerCase();
+      setText(newText);
+    }
 
 
-    const [text, setText] = useState("Enter your text here");
+    const [text, setText] = useState("");
   return (
-    <div>
-    <div class="mb-3">
+    <>
+    <div className="container mb-3">
         <h3>{props.heading}</h3>
         <textarea className="form-control" value={text} onChange={handleUpChange} id="myBox" rows="6"></textarea>
+        <button className='btn btn-primary my-2' onClick={handleUpClick}>Convert to uppercase</button>
+        <button className='btn btn-primary my-2 mx-2' onClick={handleloClick}>Convert to uppercase</button>
     </div>
-    <button className='btn btn-primary' onClick={handleUpClick}>Convert to uppercase</button>
+    <div className="container">
+      <h1>Summary of your text:</h1>
+      <p>{text.split(" ").length} words and {text.length} characters</p>
+      <p>{0.008 * text.split('').length} Minutes read </p>
+      <h3>Preview:</h3>
+      <p>{text}</p>
     </div>
+    </>
   )
 }
