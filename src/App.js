8061a7b114e9d5434745dffc694668569b345877
Shoaib Jamil <shoaibjamil43@gmail.com>
2022-08-02 14:08:59 +0500

    Adding Ant design in Package JSon

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App() {
   return (
     <>
+    <Router>
       <Navbar title="Textutils" about="About us" />
-      <div className="container my-5">
-        <TextForm heading="Enter your text to analyze:"/>
-      </div>
+      <Routes>
+      
+      <Route  path="/" element={<TextForm heading="Enter your text to analyze:"/>} exact /> 
+        
+      
+      <Route  path="/about" element={<About/>} exact /> 
+      </Routes>
+      </Router>
     </>
   );
 }
